2ded32afa2739c60d33123a0c9ff5f8ba85eb783
Luke Smith <smithmr8@gmail.com>
2015-07-30 00:11:51 +0100

    Improving the layout of the tracking page

--- view/tracking.php
+++ view/tracking.php
@@ -1,6 +1,24 @@
-<?php
-   $view = "<h1>Tracking for ziz.im/".$_GET['q']."<br /><small>(".$tracking->get_redirect_url().")</small></h1>";
-   $view .= "<p><strong>Number of clicks: </strong>" . $tracking->getClicks() . "</p>";
-   $view .= "<p><strong>Timestamp last clicked: </strong>" . $tracking->getLastClick() . "</p>";
+<html>
+  <head>
+    <style type="text/css">
+      * { font-family: Arial, Helvetica, sans-serif; }
+    </style>
+    <title>Ziz.im - Shortened URL Stats</title>
+  </head>
+  <body>
 
-   echo $view;
+  <h1>Tracking Information</h1>
+
+    <?php
+
+      $view = "<p><strong>Shortened URL:</strong> ziz.im/".rtrim($_GET['q'], "$")."</p>";
+      $view .= "<p><strong>Destination URL:</strong> ".$tracking->get_redirect_url()."</p>";
+      $view .= "<p><strong>Number of clicks: </strong>" . $tracking->getClicks() . "</p>";
+      $view .= "<p><strong>Timestamp last clicked: </strong>" . $tracking->getLastClick() . "</p>";
+
+      echo $view;
+
+    ?>
+
+  </body>
+</html>
